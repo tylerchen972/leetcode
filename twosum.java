f7ee7ac0bbfbecd94459d7d1703cbd41b7f61a91
tylerchen972 <tylerchen972@gmail.com>
2022-06-12 22:46:20 -0400

    Create twosum.java

---
+++ twosum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] sol = new int[2];
+        for(int i=0; i<nums.length; i++) {
+            for(int j=1; j<nums.length; j++) {
+                if(nums[i] + nums[j] == target && i != j) {
+                    sol[0] = j;
+                    sol[1] = i;
+                };
+            }
+        }
+    return sol;
+    }
+}
